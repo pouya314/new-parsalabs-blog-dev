bb0304d01c229c21666fcb8b7d66baf7543123d2
Pouya Arvandian <pouya314@gmail.com>
2014-10-14 11:54:02 +0800

    test whether capybara can run js with webkit driver. success \o/

---
+++ test/integration/blog_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class BlogTest < ActionDispatch::IntegrationTest
+  setup do
+    Capybara.current_driver = Capybara.javascript_driver
+  end
+
+  test 'shows blog posts' do
+    visit root_path
+    
+    within("h1") do
+      assert has_content? 'POSTS'
+    end
+  end
+
+  test "javascript works" do
+    visit root_path
+    click_button('CLICK ME')
+    
+    within("div#my-target") do
+      assert has_selector? 'p'
+      assert has_content? 'HELLO JS'
+    end
+    
+    click_button('CLICK ME')
+    assert page.has_content? 'HELLO JS', :count => 2
+  end
+end
